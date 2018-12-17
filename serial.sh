d7edbc523fdb6a2ab81587a41edb0d837e9615d5
eadys <toldtedith@gmail.com>
2018-12-17 13:51:49 +0000

    Update serial.sh

--- serial.sh
+++ serial.sh
@@ -4,16 +4,16 @@
 
 # echo $epochTime
 
-# cat /Users/sea01/Documents/repos/infrastructure-configs/bind/db.oddschecker.com
+# cat infrastructure-configs/bind/db.example.com
 
-# grep= $(grep "[0-9]+ ;" /Users/sea01/Documents/scripts/test)
+# grep= $(grep "[0-9]+ ;" /path/to/scripts/test)
 # echo $grep
 
-# cat /Users/sea01/Documents/scripts/test | awk '{gsub(/[0-9]+ ; s/, "epochTime, ; s")}'
+# cat /path/to/scripts/test | awk '{gsub(/[0-9]+ ; s/, "epochTime, ; s")}'
 
- sed -i -pe 's/[0-9]+ ;/123/g' /Users/sea01/Documents/scripts/test
+ sed -i -pe 's/[0-9]+ ;/123/g' /path/to/scripts/test
 
-# cat /Users/sea01/Documents/scripts/test
+# cat /path/to/scripts/test
 
 file=test
 
