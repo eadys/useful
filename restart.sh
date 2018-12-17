1bb76554ec17cb6e8110353f9c3227d6eda1a16d
eadys <toldtedith@gmail.com>
2018-12-17 13:50:45 +0000

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# sudo su oddschecker
-
 env=1
-cd /Users/sea01/Downloads
+cd /path/to/file
 for e in uploads uploads_test uploads_dev
 do
   dev=1
   for d in OC TA TB MA MB
   do
-    mkdir -p "home/oddschecker/$e/$d/EN/content/pages/insight/cricket"
-    # cd "/home/oddschecker/$e/$d/EN/content/pages/insight"
-    # mkdir cricket
+    mkdir -p "home/example/$e/$d/EN/content/pages/insight/sport"
     let "dev+=1"
 done
 let "env+=1"
